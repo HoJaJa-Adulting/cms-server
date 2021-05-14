687fefba39810c18b6c35c71beae4ba8d6949760
Jake Hill <jakereh91@gmail.com>
2021-05-14 15:12:39 +0200

    add db connection

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 const port = 3000;
 
+// Database stuff
+const mongoUri = ""; // Add connection string here, between the quotes
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
+
+// Server stuff
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
