b70ae05ef24fcc129cfa6b626bbff6b30606bfc3
Jake Hill <jakereh91@gmail.com>
2021-05-19 09:58:15 +0200

    complete sign in and sign up routes, with authentication

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+require("./models/User");
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
+const requireAuth = require("./middlewares/requireAuth");
 
 const app = express();
 const port = 3000;
@@ -20,10 +23,11 @@ mongoose.connection.on("error", (err) => {
 });
 
 // Server stuff
+app.use(bodyParser.json());
 app.use(authRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(port, () => {
