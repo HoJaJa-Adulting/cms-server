f23d17aa07a92d8f1b0acc5aeb33e05b8ff4f67c
Jake Hill <jakereh91@gmail.com>
2021-11-03 17:30:21 +0100

    add OPTIONSas allowed method

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ const allowCrossDomain = function (req, res, next) {
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin); // restrict it to the required domain
   }
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 };
